ddfac24109a13ef11776b79f9acadb6562c94c76
Schonhoffer <andrews@iqmetrix.com>
2012-03-02 17:05:14 -0600

    Added JQ.Mobi

---
+++ SandboxTest/MobileRedirectHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SandboxTest
+{
+	public class MobileRedirectHandler : IHttpModule
+	{
+		private string MobileSiteURL = "/mobile";
+
+		public void Init(HttpApplication app)
+		{
+			app.BeginRequest += this.OnBeginRequest;
+		}
+
+		public void Dispose() { }
+
+		public void OnBeginRequest(object o, EventArgs args)
+		{
+			HttpContext context = HttpContext.Current;
+
+			if (/* context.Request.Browser.IsMobileDevice && */
+				context.Request.RawUrl.LastIndexOf(MobileSiteURL) < 0)
+				context.Response.Redirect(MobileSiteURL, true);
+		}
+	}
+}
